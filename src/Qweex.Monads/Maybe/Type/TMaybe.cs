360aef27fe2f89ef56444096651c7c56526b9807
Giorgi Koguashvili <kogo27@gmail.com>
2018-03-20 11:22:59 +0400

    added Maybe impl over Either

---
+++ src/Qweex.Monads/Maybe/Type/TMaybe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Qweex.Monads.Types;
+using Qweex.Unions;
+
+namespace Qweex.Monads.Maybe.Type
+{
+    public abstract class TMaybe<TLeft, TRight>
+    {
+        public abstract class P<E> :
+            TUnion<TLeft, TRight>,
+            TMonad<TRight>.T<TLeft>.P<E>,
+            TFunctor<TRight>.T<TLeft>.P<E>
+        {
+            public P(Func<TUnion<TLeft, TRight>> factory) : base(factory)
+            {
+            }
+
+            public P(TLeft value) : base(value)
+            {
+            }
+
+            public P(TRight value) : base(value)
+            {
+            }
+        }
+    }
+}
---
+++ test/Qweex.Monads.Tests/MaybeMonadTests.cs
@@ -0,0 +1,39 @@
+﻿using Qweex.Monads.Maybe.Type;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Qweex.Monads.Either;
+using Qweex.Unions;
+using Xunit;
+
+namespace Qweex.Monads.Tests
+{
+    public class MaybeMonadTests
+    {
+        [Fact]
+        public static void Maybe_bind_case_1_2()
+        {
+            Assert
+                .Equal(
+                    "3",
+                    new Maybe<int>(1)
+                        .Bind(v => new Maybe<int>(v + 2))
+                        .Match(
+                            n => n.ToString(),
+                            r => r.ToString()
+                        )
+                );
+
+            Assert
+                .Equal(
+                    new Nothing().ToString(),
+                    new Maybe<int>()
+                        .Bind(v => new Maybe<int>(v + 2))
+                        .Match(
+                            n => n.ToString(),
+                            r => r.ToString()
+                        )
+                );
+        }
+    }
+}
